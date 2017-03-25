b02fbc78cf623bf30120d084986103f4af171097
indexhui <indexhui@gmail.com>
2017-03-25 16:54:20 +0800

    add hero section

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <section id="app">
 
-    <img src="./assets/logo.png">
-
+<!--     <img src="./assets/logo.png"> -->
+    <hero></hero>
     <sponser></sponser>
 
     <mycomponent></mycomponent>
@@ -13,6 +13,7 @@
 <script>
 import MyComponent from './components/myComponent.vue'
 import Sponser from './components/sponser.vue'
+import Hero from './components/hero.vue'
 export default {
   name: 'app',
   data () {
@@ -23,18 +24,23 @@ export default {
   components: {
     mycomponent: MyComponent,
     sponser: Sponser,
+    hero: Hero,
   },
 }
 </script>
 
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 h1, h2 {
---
+++ src/components/hero.vue
@@ -0,0 +1,70 @@
+<template lang="html">
+  <section>
+
+
+  <div class="hero">
+    <img class="hero_head" src="../assets/hero/hero_head.png">
+    <img class="hero_hand" src="../assets/hero/hero_hand.png">
+    <img class="hero_hand2" src="../assets/hero/hero_hand2.png">
+    <img class="hero_foot" src="../assets/hero/hero_foot.png">
+    <div class="h1">NTU<br/>ART<br/>FESTIVAL </div>
+  </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      msg1:'我是 `src/components/myComponent.vue` ',
+      msg2:'NTU'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  height: 100vh;
+  background: #6A8B9C;
+  overflow: hidden;
+  width: 100%;
+  position: relative;
+}
+
+.hero_head {
+  height: 360px;
+  position: absolute;
+  bottom: 0;
+  left: 30%;
+}
+
+.hero_hand {
+  height: 240px;
+  position: absolute;
+  bottom: 40%;
+  left: 10%;
+}
+
+.hero_hand2 {
+  height: 240px;
+  position: absolute;
+  bottom: 24%;
+  right: 10%;
+}
+
+.hero_foot {
+  height: 180px;
+  position: absolute;
+  bottom: 12%;
+  left: 7%;
+}
+
+.h1 {
+  text-align: left;
+  color: #FFF;
+  font-size: 32px;
+  margin-left: 20px;
+}
+</style>
